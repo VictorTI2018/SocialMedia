15b820689edd2a3f497e5f5e67dcfdccda9467c9
VictorTI2018 <victorhugoTI@outlook.com>
2020-01-28 14:36:49 -0300

    adicionando campos na view de publicar

--- Sistema/src/views/Publicar.vue
+++ Sistema/src/views/Publicar.vue
@@ -2,16 +2,25 @@
   <span>
     <div class="row">
       <v-grid tamanho="12">
-        <div class="row valign-wrapper">
-          <v-grid class="input-field" :tamanho="conteudo ? '10' : '12'">
-            <textarea id="textarea1" class="materialize-textarea" v-model="conteudo"></textarea>
-            <label for="textarea1">O que está pensando ?</label>
-          </v-grid>
-
-          <div class="col s2" v-if="conteudo">
-            <button class="waves-effect waves-light btn">Publicar</button>
-          </div>
-        </div>
+        <v-grid class="input-field" tamanho="12">
+          <input id="tiulo" type="text" class="validate" v-model="conteudo.titulo" />
+          <label for="tiulo">Titulo</label>
+        </v-grid>
+        <v-grid class="input-field" tamanho="12" v-if="conteudo.titulo">
+          <textarea id="textarea1" class="materialize-textarea" v-model="conteudo.texto"></textarea>
+          <label for="textarea1">O que está pensando ?</label>
+        </v-grid>
+        <v-grid class="input-field" tamanho="12" v-if="conteudo.texto">
+          <input id="link" type="text" class="validate" v-model="conteudo.link" />
+          <label for="link">Link</label>
+        </v-grid>
+        <v-grid class="input-field" tamanho="12" v-if="conteudo.link">
+          <input id="imagem" type="text" class="validate" v-model="conteudo.Imagem" />
+          <label for="imagem">Imagem</label>
+        </v-grid>
+        <v-grid class="col s2 offset-s10" v-if="conteudo.titulo">
+          <button class="waves-effect waves-light btn">Publicar</button>
+        </v-grid>
       </v-grid>
     </div>
   </span>
@@ -22,7 +31,12 @@ export default {
   name: "publicar",
   data() {
     return {
-      conteudo: ""
+      conteudo: {
+        titulo: "",
+        texto: "",
+        link: "",
+        imagem: ""
+      }
     };
   }
 };
